fd2155b938d9757f843f9a6b32cffe7723efbca1
XenoProxy <XenoProxy@yandex.by>
2023-06-14 15:44:33 +0300

    first commit

---
+++ database/migrations/2023_06_14_114922_create_doctors_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('poly_id');
+            $table->string('name');
+            $table->string('field');
+            $table->timestamps();
+
+            $table->foreign('poly_id')->references('id')->on('polyclinics')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
