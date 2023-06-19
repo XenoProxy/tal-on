e214a5d900051d1448af91038474002b4cbd3d38
XenoProxy <XenoProxy@yandex.by>
2023-06-19 12:56:02 +0300

    doctors table filter

--- public/js/ticket.js
+++ public/js/ticket.js
@@ -5,18 +5,37 @@ $(document).ready(function() {
     // при выборе элемента из списка срабатывает событие
     $('#polyclinic').change(function(){
         polyclinic = $('#polyclinic option:selected').text();
-        $('#js-result_p').html('Результат: ' + polyclinic);
+        $('#js-result_p').html(' ' + polyclinic);
     });
 
     $('#doctor_field').change(function(){
         field = $('#doctor_field option:selected').text();
-        $('#js-result_f').html('Результат: ' + field);
+        $('#js-result_f').html(' ' + field);
     });
 
-    $('#select').on('click',function(){
-        // получаем значение из выбранного селекта
-        //var optionsValue = this.options[this.selectedIndex].value;
-        // добавляем выбранное значение в input
-        //$('#test').val(optionsValue);
+
+    $('.table-filters select').on('change', function () {
+        filterTable($(this).parents('table'));
     });
+
+    function filterTable($table) {
+        var $filters = $table.find('.table-filters td');
+        var $rows = $table.find('.table-data');
+        $rows.each(function (rowIndex) {
+            var valid = true;
+            $(this).find('td').each(function (colIndex) {
+                if ($filters.eq(colIndex).find('select').val()) {
+                    if ($(this).html().toLowerCase().indexOf(
+                    $filters.eq(colIndex).find('select').val().toLowerCase()) == -1) {
+                        valid = valid && false;
+                    }
+                }
+            });
+            if (valid === true) {
+                $(this).css('display', '');
+            } else {
+                $(this).css('display', 'none');
+            }
+        });
+    }
 });
--- resources/views/tickets/create.blade.php
+++ resources/views/tickets/create.blade.php
@@ -30,39 +30,62 @@
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
-                    <strong>Polyclinic:</strong></br>
-                    <select name="polyclinic" id="polyclinic" class="custom-select">
-                        @foreach($polyclinics as $policlynic)
-                            <option value="{{ $policlynic->name }}"> {{ $policlynic->name }} </option>
-                        @endforeach
-                    </select></br>
-                    <div class="col-sm" id="js-result_p">
-                        Результат: 
-                    </div>
+                    <strong>Polyclinic:</strong>                                         
+                    <div class="col-sm" id="js-result_p"></div>
                 </div>
             </div></br>      
 
 
             <div class="col-xs-12 col-sm-12 col-md-18">
                 <div class="form-group">
-                    <strong>Doctor:</strong></br>      
-                    <select name="doctor_field" id="doctor_field"  class="custom-select ">
-                        @foreach($doctors as $doctor)
-                            <option class="field" value="{{ $doctor->field }}"> {{ $doctor->field }} </option>
-                        @endforeach
-                    </select></br>
-                    <div class="col-sm" id="js-result_f">
-                        Результат: 
-                    </div></br>
-
-
-
-                    <select name="doctor_name" id="doctor_name" class="custom-select">
-                        @foreach($doctors as $doctor)
-                            <option value="{{ $doctor->name }}"> {{ $doctor->name }} </option>
-                        @endforeach
-                    </select>
+                    <strong>Doctor:</strong>                
+                    <div class="col-sm" id="js-result_f"></div>
                 </div>
+            </div>
+
+
+            <table class="table table-bordered" id="filter-table">
+                <tr>
+                    <th>Name</th>
+                    <th>Field</th>
+                    <th>Polyclinic</th>
+                </tr>
+                <tr class='table-filters'>
+                    <td>
+                        
+                    </td>
+                    <td>
+                        <select name="doctor_field" id="doctor_field"  class="custom-select ">
+                            <option value=""> </option>
+                            @foreach($doctors as $doctor)
+                                <option class="field" value="{{ $doctor->field }}"> {{ $doctor->field }} </option>
+                            @endforeach
+                        </select></br>
+                    </td>
+                    <td>
+                        <select name="polyclinic" id="polyclinic" class="custom-select">
+                            <option value=""> </option>
+                            @foreach($polyclinics as $policlynic)
+                                <option value="{{ $policlynic->name }}"> {{ $policlynic->name }} </option>
+                            @endforeach
+                        </select></br>
+                    </td>
+                </tr>
+                
+                @foreach ($doctors as $doctor)
+                <tr class='table-data'>
+                    <td>{{ $doctor->name }}</td>
+                    <td>{{ $doctor->field }}</td>
+                    <td>
+                        <a href="{{ route('polyclinics.show', $doctor->poly_id) }}">{{ $doctor->polyclinic->name }}</a>
+                    </td>
+                    <td>
+                        <a class="btn btn-info" href="{{ route('doctors.show', $doctor->id) }}">Select</a>
+                    </td>
+                </tr>
+                @endforeach
+            </table>
+
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
