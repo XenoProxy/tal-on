951a5653923428a2dce8aca122d7b8c7ad8a12a5
XenoProxy <XenoProxy@yandex.by>
2023-06-21 11:04:35 +0300

    card number of user

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -14,6 +14,12 @@
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
+                <strong>Card Number:</strong>
+                {{ $user->id }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
                 <strong>Name:</strong>
                 {{ $user->name }}
             </div>
