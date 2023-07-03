ac59d48171677aad237a94d33292693bb7ce549a
XenoProxy <XenoProxy@yandex.by>
2023-07-03 10:21:34 +0300

    polyclinic show clean

--- app/Http/Controllers/PolyclinicController.php
+++ app/Http/Controllers/PolyclinicController.php
@@ -26,28 +26,30 @@ class PolyclinicController extends Controller
         return view('polyclinics.index', compact('polyclinics', 'contacts'));
     }    
 
-    public function show(Polyclinic $polyclinic, Appointment $appointments)
+    public function show(Polyclinic $polyclinic)
     {
         // врачи текущей поликлиники
         $polyclinic_doctors = Doctor::where('poly_id', $polyclinic->id)->get();
 
+        $appointments = Appointment::all();
+        foreach($appointments as $appointment){
+                                            
+        }
+        //dd($appointments);
+
         $date_arr = [];
-        //$current_date = date("d F Y");
         for($i = 0; $i <= 14; $i++){
             $date_arr[] = date("Y-m-d", time() + 86400*$i);
         }
 
-        //dd(strtotime('08:00')); // 1687939200
-        //dd(date('H:i', 1687939200+900)); // 08:15
-
         $time_arr = [];
-        //$current_time = date("H:i");
         for($i = 0; $i <= 20; $i++){
             $time_arr[] = date("H:i", 1687939200 + 900*$i);
         }
 
         $contacts = $this->contactsService->phoneNumber();
         return view('polyclinics.show', compact(
+            'appointments',
             'date_arr', 
             'polyclinic', 
             'polyclinic_doctors', 
