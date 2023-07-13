64888aa1a3877fea9879486b79b0ca70dc821b68
XenoProxy <XenoProxy@yandex.by>
2023-07-13 14:34:12 +0300

    admin index

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,1 +1,57 @@
-Admin Panel
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div id="isLiked" style="display:none;"></div>
+            <div class="pull-left">
+                <h2>Admin Panel</h2>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">        
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <a class="btn btn-info" href="{{ route('polyclinics.index') }}">Add Polyclinic</a>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <a class="btn btn-info" href="{{ route('doctors.index') }}">Add Doctor</a>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-8">
+            <div class="card">
+                <div class="card-header"><strong>{{ __('My appointments') }}</strong></div>
+                
+                <div class="card-body">    
+                    <table class="table table-bordered">
+                        <tr>
+                            <th>№</th>
+                            <th>Polyclinic</th>
+                            <th>Doctor</th>
+                            <th>Office</th>
+                            <th>Date</th>
+                            <th>Time</th>
+                            <th>Comments</th>
+                        </tr>
+                        @foreach ($appointments as $appointment)
+                        <tr>
+                            <td>{{ $appointment->id }}</td>
+                            <td>{{ $polyclinics[$appointment->doctor->poly_id-1]["name"] }}</td>
+                            <td>{{ $appointment->doctor->name }}</td>
+                            <td>{{ $appointment->doctor->office }}</td>
+                            <td>{{ date("l - d F Y", strtotime($appointment->date)) }}</td>
+                            <td> {{ date("H:i", strtotime($appointment->time)) }}</td>
+                            <td> {{ $appointment->comments }}</td>
+                        </tr>
+                        @endforeach
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>  
+
+@endsection
