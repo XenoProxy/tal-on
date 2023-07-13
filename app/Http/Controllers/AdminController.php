d05113856e8908afcf0008168bcf448dd2f0258d
XenoProxy <XenoProxy@yandex.by>
2023-07-13 14:29:17 +0300

    admin controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
+use App\Models\Appointment;
+use App\Models\Polyclinic;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('admin.index');
+        $user = User::find(Auth::id());
+        $appointments = Appointment::where('user_id', $user->id)->get();
+        $polyclinics = Polyclinic::all()->toArray();
+        return view('admin.index', compact('user', 'appointments', 'polyclinics'));
     } 
 }
