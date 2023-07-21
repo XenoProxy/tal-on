cc5aac0f48480b05f19668ccabe3b18aa6ccbb5f
XenoProxy <XenoProxy@yandex.by>
2023-07-21 11:36:58 +0300

    bootstrap + sass from vite -

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,13 +9,11 @@
 
     <title>{{ __('Tal-On') }}</title>
 
-    <!-- Fonts -->
+    <!-- Styles -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="dns-prefetch" href="//fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-    <!-- Scripts -->
-    
+    <!-- Scripts -->    
     <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
     <script src="{{ asset('js/filterDoctors.js') }}"></script>
     <script src="{{ asset('js/getDoctorAppointment.js') }}"></script>
