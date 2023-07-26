825d01b7cca76583188de023a732a999fd9499eb
XenoProxy <XenoProxy@yandex.by>
2023-07-26 13:55:39 +0300

    home banner and success btn

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    
+<div class="container">    
     <div class="home-container">        
         <div class="home-info">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In arcu cursus euismod quis. Sed risus ultricies tristique nulla aliquet enim tortor at. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Aliquam id diam maecenas ultricies mi eget mauris. Sed pulvinar proin gravida hendrerit lectus. Bibendum ut tristique et egestas quis ipsum. Nulla aliquet enim tortor at. Lorem donec massa sapien faucibus. Amet volutpat consequat mauris nunc congue nisi. Arcu dui vivamus arcu felis bibendum ut tristique. Phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet enim.
             At auctor urna nunc id cursus metus aliquam eleifend. Congue quisque egestas diam in arcu cursus euismod quis viverra. Tristique magna sit amet purus gravida quis blandit turpis cursus. Ipsum suspendisse ultrices gravida dictum fusce. Amet est placerat in egestas erat imperdiet sed. Venenatis tellus in metus vulputate eu. Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Bibendum arcu vitae elementum curabitur vitae nunc. Blandit volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque. Quam quisque id diam vel quam elementum pulvinar etiam. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Nunc sed velit dignissim sodales ut eu. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Eu augue ut lectus arcu.
         </div>
-        <img class="home-banner" src="/images/doctor-hands-in-green-latex-gloves-hold-phone.jpg">
         <a class="btn btn-info order-home" href="{{ route('polyclinics.index') }}">Order the ticket</a>
     </div>      
 </div>
