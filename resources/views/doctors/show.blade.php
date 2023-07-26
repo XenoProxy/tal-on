6db2ea6144a9d3bbf250c0aaacb89ab1fe6e39ea
XenoProxy <XenoProxy@yandex.by>
2023-07-26 15:56:37 +0300

    doctor show layout

--- resources/views/doctors/show.blade.php
+++ resources/views/doctors/show.blade.php
@@ -1,15 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="doctor-container"> 
-    <h2 class="show-doctor doctor-name">{{ $doctor->name }}</h2>
+<div class="container">
+    
 
     <div class="pull-right">
         <a class="btn btn-primary btn-back" href="{{ route('doctors.index') }}"> Back</a>
     </div>
-
-    <div class="doctor-info">
-                
+    <div class="doctor-container">    
+    <h2 class="show-doctor doctor-name">{{ $doctor->name }}</h2>     
+        <img class="doctor-photo" src="https://via.placeholder.com/250x300" alt="">
+                    
         <div class="info-item">
             <strong>Field:</strong>
             {{ $doctor->field }}
@@ -23,13 +24,9 @@
         <div class="info-item">
             <strong>Polyclinic:</strong>
             <a href="{{ route('polyclinics.show', $doctor->poly_id) }}">{{ $polyclinic_name }}</a>                
-        </div>        
-
-    </div>
-     
-    <div class="doctor-summary">
-        <strong>Info:</strong>
-        {{ $doctor->info }}
-    </div>
-</div>    
+        </div>    
+        
+        <div class="doctor-summary">{{ $doctor->info }}</div>
+    </div>    
+</div>
 @endsection
