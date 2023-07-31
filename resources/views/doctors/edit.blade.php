64e0faa98e224256bf4bf0358c17a587bc4d4e33
XenoProxy <XenoProxy@yandex.by>
2023-07-31 14:56:50 +0300

    doctor edit layout

--- resources/views/doctors/edit.blade.php
+++ resources/views/doctors/edit.blade.php
@@ -1,48 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Edit Doctor</h2>
-        </div>
+<div class="container">
+    <div class="edit-container">
+        <h2 class="edit-create-head">Edit Doctor</h2>
         <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('doctors.index') }}"> Back</a>
+            <a class="btn btn-primary btn-back" href="{{ route('polyclinics.index') }}"> Back</a>
         </div>
-    </div>
-</div>
-<form action="{{ route('doctors.update', $doctor->id) }}" method="post">
-    @csrf
-    @method('PUT')
-    
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
+
+        <form action="{{ route('doctors.update', $doctor->id) }}" method="post">
+            @csrf
+            @method('PUT')            
+            <div class="form-container polyclinic-form">
+                <div class="form-item">
                     <strong>Name:</strong>
                     <input type="text" name="name" value="{{ $doctor->name }}" class="form-control" placeholder="Name">
                 </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
+
+                <div class="form-item">
                     <strong>Field:</strong>
                     <input type="text" name="field" value="{{ $doctor->field }}" class="form-control" placeholder="Field">
                 </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
+
+                <div class="form-item">
                     <strong>Office:</strong>
                     <input type="text" name="office" value="{{ $doctor->office }}" class="form-control" placeholder="Office">
                 </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
+
+                <div class="form-item">
                     <strong>Info:</strong>
-                    <textarea name="info" style="height:300px" class="" placeholder="Info">{{ $doctor->info }}</textarea>
-                </div>
+                    <textarea name="info" style="height:300px" class="form-control" placeholder="Info">{{ $doctor->info }}</textarea>
+                </div>  
+                            
+                <button type="submit" class="btn btn-primary admin-submit-btn">Submit</button>
             </div>
-            <div class="col-xs-12 col-sm-12 col-md-12 text-center">              
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-        </div>
-</form>
+        </form>
+    </div>
+</div>
 @endsection
