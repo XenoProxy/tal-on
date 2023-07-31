3e8490205cbac97081e1865109e5a8d72b0cba92
XenoProxy <XenoProxy@yandex.by>
2023-07-31 14:52:52 +0300

    edit polyclinic layout

--- resources/views/polyclinics/edit.blade.php
+++ resources/views/polyclinics/edit.blade.php
@@ -1,42 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Edit Polyclinic</h2>
-        </div>
+<div class="container">
+    <div class="edit-container">
+        <h2 class="edit-create-head">Edit Polyclinic</h2>
         <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('polyclinics.index') }}"> Back</a>
+            <a class="btn btn-primary btn-back" href="{{ route('polyclinics.index') }}"> Back</a>
         </div>
+
+        <form action="{{ route('polyclinics.update', $polyclinic->id) }}" method="post">
+            @csrf
+            @method('PUT')            
+                <div class="form-container polyclinic-form">
+                        <div class="form-item">
+                            <strong>Name:</strong>
+                            <input type="text" name="name" value="{{ $polyclinic->name }}" class="form-control" placeholder="Name">
+                        </div> 
+
+                        <div class="form-item">
+                            <strong>Address:</strong>
+                            <input type="text" class="form-control" name="address" placeholder="Address" value="{{ $polyclinic->address }}">
+                        </div>
+                    
+                        <div class="form-item">
+                            <strong>Contacts:</strong>
+                            <input type="tel" name="contacts" value="{{ $polyclinic->contacts }}" class="form-control" placeholder="Contacts">
+                        </div>
+                                 
+                        <button type="submit" class="btn btn-primary admin-submit-btn">Submit</button>
+                </div>
+        </form>
     </div>
 </div>
-<form action="{{ route('polyclinics.update', $polyclinic->id) }}" method="post">
-    @csrf
-    @method('PUT')
-    
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Name:</strong>
-                    <input type="text" name="name" value="{{ $polyclinic->name }}" class="form-control" placeholder="Name">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Address:</strong>
-                    <input type="text" class="form-control" name="address" placeholder="Address" value="{{ $polyclinic->address }}">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Contacts:</strong>
-                    <input type="tel" name="contacts" value="{{ $polyclinic->contacts }}" class="form-control" placeholder="Contacts">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12 text-center">              
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-        </div>
-</form>
 @endsection
