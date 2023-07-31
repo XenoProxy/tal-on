6ff8057b0e93dd3b00f9e404ef598787c8ced012
XenoProxy <XenoProxy@yandex.by>
2023-07-31 14:00:15 +0300

    account appointments table layout and check

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -2,60 +2,52 @@
 
 @section('content')
 
-    <div class="row">
-        <div class="center">
-            <h2 class="account-head">My Account</h2>
-        </div>
+    <div class="container">     
+        <div class="table-container">
+            <div class="my-appointments">
+                <h2 class="appointments-table-head">{{ __('My appointments') }}</h2> 
+            </div>  
 
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
+            <div class="user-info">
+                <div class="info-item">
                     <strong>Card Number:</strong>
                     {{ $user->id }}
                 </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
+                <div class="info-item">
                     <strong>Name:</strong>
                     {{ $user->name }}
                 </div>
             </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <a class="btn btn-info" href="{{ route('polyclinics.index') }}">Order the ticket</a>
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-8">
-                <div class="card">
-                    <div class="card-header"><strong>{{ __('My appointments') }}</strong></div>
-                    
-                    <div class="card-body">    
-                        <table class="table table-bordered">
-                            <tr>
-                                <th>№</th>
-                                <th>Polyclinic</th>
-                                <th>Doctor</th>
-                                <th>Office</th>
-                                <th>Date</th>
-                                <th>Time</th>
-                                <th>Comments</th>
-                            </tr>
-                            @foreach ($appointments as $appointment)
-                            <tr>
-                                <td>{{ $appointment->id }}</td>
-                                <td>{{ $polyclinics[$appointment->doctor->poly_id-1]["name"] }}</td>
-                                <td>{{ $appointment->doctor->name }}</td>
-                                <td>{{ $appointment->doctor->office }}</td>
-                                <td>{{ date("l - d F Y", strtotime($appointment->date)) }}</td>
-                                <td> {{ date("H:i", strtotime($appointment->time)) }}</td>
-                                <td> {{ $appointment->comments }}</td>
-                            </tr>
-                            @endforeach
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>  
-    </div>
+            
+            <div class="my-appointments">
+                <a class="btn btn-info order-home" href="{{ route('polyclinics.index') }}">Order the ticket</a>
+            </div> 
+
+            @if (count($appointments) !== 0)
+                <table class="table table-appointments">                
+                    <tr>
+                        <th>№</th>
+                        <th>Polyclinic</th>
+                        <th>Doctor</th>
+                        <th>Office</th>
+                        <th>Date</th>
+                        <th>Time</th>
+                        <th>Comments</th>
+                    </tr>
+                    @foreach ($appointments as $appointment)
+                    <tr>
+                        <td>{{ $appointment->id }}</td>
+                        <td>{{ $polyclinics[$appointment->doctor->poly_id-1]["name"] }}</td>
+                        <td>{{ $appointment->doctor->name }}</td>
+                        <td>{{ $appointment->doctor->office }}</td>
+                        <td>{{ date("l - d F Y", strtotime($appointment->date)) }}</td>
+                        <td> {{ date("H:i", strtotime($appointment->time)) }}</td>
+                        <td> {{ $appointment->comments }}</td>
+                    </tr>
+                    @endforeach
+                </table>
+            @endif            
+        </div>
+    </div> 
 
 @endsection
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,37 +2,36 @@
 
 @section('content')
 
-    <div class="container">     
-         
-
+    <div class="container">    
         <div class="table-container">
             <div class="my-appointments">
                 <h2 class="appointments-table-head">{{ __('My appointments') }}</h2> 
             </div>  
-
-            <table class="table table-appointments">
-                
-                <tr>
-                    <th>№</th>
-                    <th>Polyclinic</th>
-                    <th>Doctor</th>
-                    <th>Office</th>
-                    <th>Date</th>
-                    <th>Time</th>
-                    <th>Comments</th>
-                </tr>
-                @foreach ($appointments as $appointment)
-                <tr>
-                    <td>{{ $appointment->id }}</td>
-                    <td>{{ $polyclinics[$appointment->doctor->poly_id-1]["name"] }}</td>
-                    <td>{{ $appointment->doctor->name }}</td>
-                    <td>{{ $appointment->doctor->office }}</td>
-                    <td>{{ date("l - d F Y", strtotime($appointment->date)) }}</td>
-                    <td> {{ date("H:i", strtotime($appointment->time)) }}</td>
-                    <td> {{ $appointment->comments }}</td>
-                </tr>
-                @endforeach
-            </table>            
+            
+            @if (count($appointments) !== 0)
+                <table class="table table-appointments">                
+                    <tr>
+                        <th>№</th>
+                        <th>Polyclinic</th>
+                        <th>Doctor</th>
+                        <th>Office</th>
+                        <th>Date</th>
+                        <th>Time</th>
+                        <th>Comments</th>
+                    </tr>
+                    @foreach ($appointments as $appointment)
+                    <tr>
+                        <td>{{ $appointment->id }}</td>
+                        <td>{{ $polyclinics[$appointment->doctor->poly_id-1]["name"] }}</td>
+                        <td>{{ $appointment->doctor->name }}</td>
+                        <td>{{ $appointment->doctor->office }}</td>
+                        <td>{{ date("l - d F Y", strtotime($appointment->date)) }}</td>
+                        <td> {{ date("H:i", strtotime($appointment->time)) }}</td>
+                        <td> {{ $appointment->comments }}</td>
+                    </tr>
+                    @endforeach
+                </table>
+            @endif           
         </div>
     </div>  
 
