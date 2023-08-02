75b1c50d9aa51d1ced56f4357914ace1e2015231
XenoProxy <XenoProxy@yandex.by>
2023-08-02 09:42:26 +0300

    create new polyclinic layout

--- resources/views/polyclinics/create.blade.php
+++ resources/views/polyclinics/create.blade.php
@@ -1,54 +1,47 @@
 @extends ('layouts.app')
 
 @section('content')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Add New Polyclinic</h2>
-        </div>
-        <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('polyclinics.index') }}"> Back</a>
-        </div>
+<div class="container">
+    <div class="edit-container">
+            <h2 class="edit-create-head">Add New Polyclinic</h2>
+            <div class="pull-right">
+                <a class="btn btn-primary btn-back" href="{{ route('polyclinics.index') }}"> Back</a>
+            </div>
+
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <form action="{{ route('polyclinics.store') }}" method="post">
+            @csrf
+            <div class="form-container polyclinic-form">
+                <div class="row">
+                    <div class="form-item">
+                        <strong>Name:</strong>
+                        <input type="text" name="name" class="form-control" placeholder="Name">
+                    </div>
+                
+                    <div class="form-item">
+                        <strong>Address:</strong>
+                        <input type="text" class="form-control" name="detail" placeholder="Address"></input>
+                    </div>
+                
+                    <div class="form-item">
+                        <strong>Contacts:</strong>
+                        <input type="tel" name="contacts" class="form-control" placeholder="Contacts">
+                    </div>
+                
+                    <button type="submit" class="btn btn-primary admin-submit-btn">Submit</button>
+                </div>
+            </div>
+        </form>
     </div>
 </div>
-
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <strong>Whoops!</strong> There were some problems with your input.<br><br>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-
-<form action="{{ route('polyclinics.store') }}" method="post">
-    @csrf
-
-     <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Name:</strong>
-                <input type="text" name="name" class="form-control" placeholder="Name">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Address:</strong>
-                <input type="text" class="form-control" name="detail" placeholder="Address"></input>
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Contacts:</strong>
-                <input type="tel" name="contacts" class="form-control" placeholder="Contacts">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
-    </div>
-
-</form>
 @endsection
